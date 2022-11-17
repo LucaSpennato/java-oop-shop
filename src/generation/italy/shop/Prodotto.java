a52005f716f3b2a606f8160176afab3a0bea6fb6
Luca Spennato <luca.spennato@gmail.com>
2022-11-17 13:02:50 +0100

    variables and constructor

--- src/generation/italy/shop/Prodotto.java
+++ src/generation/italy/shop/Prodotto.java
@@ -1,5 +1,19 @@
 package generation.italy.shop;
 
 public class Prodotto {
+	
+	int code;
+	String name;
+	String description;
+	int price;
+	int iva;
+	
+	public Prodotto(String name, String description, int price) {
+		
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		
+	}
 
 }
